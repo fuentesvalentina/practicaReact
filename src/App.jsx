41c3acd945e7351d85440af93ff2bc323354e3f7
Valentina Fuentes <fuentesvalentina74@gmail.com>
2024-02-04 14:07:31 -0300

    agregamos la libreria de bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
-
+// aqui agregamos la logica del componente
   return (
-    <h1>prueba</h1>
+    //aqui puedo escribir un poco mas de logica
+    <h1 className="text-center display-3">
+      {/* aqui agrego todo el maquetado del componente */}
+      prueba</h1>
   )
 }
 
